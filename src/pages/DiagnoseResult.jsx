d05b96661c14ca7cc639e095bbf663ccf192d5b1
GioCharro <georgiocharro@hotmail.com>
2024-04-02 14:42:51 -0700

    DiagnoseResult changes

--- src/pages/DiagnoseResult.jsx
+++ src/pages/DiagnoseResult.jsx
@@ -12,9 +12,15 @@ import markerIcon from '../assets/svg/markerIcon.svg'
 import  HeartDiseaseIcon  from '../assets/svg/heartDiseaseIcon.svg';
 import LinearProgress from '@mui/material/LinearProgress';
 import CloseIcon from '@mui/icons-material/Close';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import InformationCircle from '../assets/svg/infoCircle.svg'
 import  HealthIconDisease from '../assets/svg/HealthIconDisease.svg'
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import { Modal, Box, IconButton, Typography } from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import InfoIcon from '@mui/icons-material/Info'
+
 import 'leaflet/dist/leaflet.css';
 function DiagnoseResult() {
 
@@ -137,7 +143,13 @@ function DiagnoseResult() {
             <p className='font-bold text-gray-700 ml-2 text-lg'>{diagnose.First_Suggested_Disease_Name}</p>
             <img src={InformationCircle} className='h-5 w-5 ml-3' alt="Visibility Icon" />
             <button onClick={() => handleOpenModal('first')} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
-                 Details
+              
+                 Details 
+               </button>
+               <LocalHospitalIcon className='-ml-2' />
+               <button onClick={() => handleOpenModal('firstTreatment')} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
+              
+                 Treatment
                </button>
                <Modal
           open={currentDisease !==null}
@@ -150,11 +162,13 @@ function DiagnoseResult() {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: 400,
+            width: 'auto', // Adjust width as needed or set a specific value
+            maxWidth: '90%', // Prevents the modal from being too wide on small screens
+            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
             bgcolor: 'background.paper',
             boxShadow: 24,
             p: 4,
-            borderRadius: 2,
+            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
           }}>
             <IconButton
               aria-label="close"
@@ -171,7 +185,7 @@ function DiagnoseResult() {
               Details
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              {currentDisease === 'first' && 'Details about the first suggested disease...'}
+              {currentDisease === 'first'}
             </Typography>
           </Box>
         </Modal>     
@@ -186,6 +200,7 @@ function DiagnoseResult() {
               </Box>
             </Box>
           </div>
+          
         </div>
 
         {diagnose.Second_Suggested_Disease_Name && (
@@ -208,11 +223,13 @@ function DiagnoseResult() {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: 400,
+            width: 'auto', // Adjust width as needed or set a specific value
+            maxWidth: '90%', // Prevents the modal from being too wide on small screens
+            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
             bgcolor: 'background.paper',
             boxShadow: 24,
             p: 4,
-            borderRadius: 2,
+            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
           }}>
             <IconButton
               aria-label="close"
@@ -230,7 +247,99 @@ function DiagnoseResult() {
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
               {/* Here you can place the content or details you want to show in the modal */}
-              {currentDisease === 'first' ? 'Details about the first suggested disease...' : 'Details about the second suggested disease...'}
+              {currentDisease === 'first' ? (<><div className='flex items-center'>
+                <InfoIcon style={{color:'#10B981'}} />
+                <p className='text-gray-700 font-bold text-xl ml-2'>Disease Description</p>
+                
+
+
+
+              </div>
+              <div className='flex'>
+
+              <p className='text-gray-600 font-semibold text-l p-4 '>{diagnose.First_Suggested_Disease_Description}</p>
+
+              </div>
+
+              <div className="flex  mt-2 items-center">
+
+              <LightbulbIcon style={{color:'#10B981'}}  />
+              <p className='text-gray-700 font-bold text-xl ml-2'>Prevention Tips</p>
+              </div>
+              <div className='p-4 flex font-semibold text-gray-600 '>
+              <List sx={{ pl: 0 }}>
+  {diagnose.First_Suggested_Disease_Treatment.prevention.map((tip, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {tip}
+    </ListItem>
+  ))}
+</List>
+                
+              </div>
+
+              
+
+              </>) : <><div className='flex items-center'>
+                <InfoIcon style={{color:'#10B981'}} />
+                <p className='text-gray-700 font-bold text-xl ml-2'>Disease Description</p>
+                
+
+
+
+              </div>
+              <div className='flex'>
+
+              <p className='text-gray-600 font-semibold text-l p-4 '>{diagnose.Second_Suggested_Disease_Description}</p>
+
+              </div>
+
+              <div className="flex  mt-2 items-center">
+
+              <LightbulbIcon style={{color:'#10B981'}}  />
+              <p className='text-gray-700 font-bold text-xl ml-2'>Prevention Tips</p>
+              </div>
+              <div className='p-4 flex font-semibold text-gray-600 '>
+              <List sx={{ pl: 0 }}>
+  {diagnose.Second_Suggested_Disease_Treatment.prevention.map((tip, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {tip}
+    </ListItem>
+  ))}
+</List>
+                
+              </div>
+
+              
+
+              </>}
             </Typography>
           </Box>
         </Modal>      
