97781f181ab8bc3ca21996f9d2bfc436fbc1fc02
GioCharro <georgiocharro@hotmail.com>
2024-04-02 15:55:51 -0700

    Added Treatments Bar and Info to DiagnoseResult

--- src/pages/DiagnoseResult.jsx
+++ src/pages/DiagnoseResult.jsx
@@ -18,6 +18,8 @@ import  HealthIconDisease from '../assets/svg/HealthIconDisease.svg'
 import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import { Modal, Box, IconButton, Typography } from '@mui/material';
 import ListItem from '@mui/material/ListItem';
+import RecyclingIcon from '@mui/icons-material/Recycling';
+import ScienceIcon from '@mui/icons-material/Science';
 import List from '@mui/material/List';
 import InfoIcon from '@mui/icons-material/Info'
 
@@ -27,10 +29,10 @@ function DiagnoseResult() {
     const [diagnose, setDiagnose] = useState(null)
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentDisease, setCurrentDisease] = useState(null);
-    const handleOpenModal = (diseaseKey) => {setCurrentDisease(diseaseKey)}
-    const handleCloseModal = () => {setCurrentDisease(null);}
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+    const handleOpenModal = (content) => {setModalContent(content); setModalOpen(true)}
+    const handleCloseModal = () => {setModalContent(null); setModalOpen(false)}
     const params=useParams()
     const auth=getAuth()
     const navigate=useNavigate()
@@ -142,112 +144,7 @@ function DiagnoseResult() {
             <img src={HeartDiseaseIcon} className='h-7 w-7' alt="Heart Disease Icon" />
             <p className='font-bold text-gray-700 ml-2 text-lg'>{diagnose.First_Suggested_Disease_Name}</p>
             <img src={InformationCircle} className='h-5 w-5 ml-3' alt="Visibility Icon" />
-            <button onClick={() => handleOpenModal('first')} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
-              
-                 Details 
-               </button>
-               <LocalHospitalIcon className='-ml-2' />
-               <button onClick={() => handleOpenModal('firstTreatment')} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
-              
-                 Treatment
-               </button>
-               <Modal
-          open={currentDisease !==null}
-          onClose={handleCloseModal}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 'auto', // Adjust width as needed or set a specific value
-            maxWidth: '90%', // Prevents the modal from being too wide on small screens
-            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
-            bgcolor: 'background.paper',
-            boxShadow: 24,
-            p: 4,
-            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
-          }}>
-            <IconButton
-              aria-label="close"
-              onClick={handleCloseModal}
-              sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Details
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              {currentDisease === 'first'}
-            </Typography>
-          </Box>
-        </Modal>     
-          </div>
-          <div className='card  p-4 self-stretch'>
-            <Box display="flex" alignItems="center">
-              <Box width="100%" mr={1}>
-                <LinearProgress variant="determinate" value={probabilityPercent} sx={{'& .MuiLinearProgress-bar': { backgroundColor: '#EF4444' }, }} />
-              </Box>
-              <Box minWidth={35}>
-                <Typography variant="body2" sx={{fontWeight:'bold',color:'black'}}>{`${Math.round(probabilityPercent)}%`}</Typography>
-              </Box>
-            </Box>
-          </div>
-          
-        </div>
-
-        {diagnose.Second_Suggested_Disease_Name && (
-          <div className='flex flex-col items-start justify-between  p-4 '>
-            <div className='flex items-center pb-4'>
-              <img src={HeartDiseaseIcon} className='h-7 w-7' alt="Heart Disease Icon" />
-              <p className='font-bold text-gray-700 ml-2 text-lg'>{diagnose.Second_Suggested_Disease_Name}</p>
-              <img src={InformationCircle} className='h-5 w-5 ml-3' alt="Visibility Icon" />
-              <button onClick={() => handleOpenModal('second')} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
-                 Details
-               </button>
-               <Modal
-          open={currentDisease !== null}
-          onClose={handleCloseModal}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 'auto', // Adjust width as needed or set a specific value
-            maxWidth: '90%', // Prevents the modal from being too wide on small screens
-            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
-            bgcolor: 'background.paper',
-            boxShadow: 24,
-            p: 4,
-            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
-          }}>
-            <IconButton
-              aria-label="close"
-              onClick={handleCloseModal}
-              sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Details
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              {/* Here you can place the content or details you want to show in the modal */}
-              {currentDisease === 'first' ? (<><div className='flex items-center'>
+            <button onClick={() => setModalContent(<><div className='flex items-center'>
                 <InfoIcon style={{color:'#10B981'}} />
                 <p className='text-gray-700 font-bold text-xl ml-2'>Disease Description</p>
                 
@@ -293,7 +190,149 @@ function DiagnoseResult() {
 
               
 
-              </>) : <><div className='flex items-center'>
+              </>)} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
+              
+                 Details 
+               </button>
+               <LocalHospitalIcon className='-ml-2' />
+               <button onClick={() => handleOpenModal(<>
+               
+               {diagnose.First_Suggested_Disease_Treatment.biological && <>
+               
+               
+               <div className='flex items-center -ml-2'>
+                  <RecyclingIcon />
+                  <p className='ml-2 text-xl font-bold text-green-500'>Biological Treatment</p>
+
+               </div>
+               
+                <ul>  
+                
+                
+                {diagnose.First_Suggested_Disease_Treatment.biological.map((treatment, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {treatment}
+    </ListItem>
+  ))}
+                
+                
+                </ul> </>}
+               
+               
+                {diagnose.First_Suggested_Disease_Treatment.chemical && <>
+               
+               
+               <div className='flex items-center -ml-2 mt-2'>
+                  <ScienceIcon />
+                  <p className='ml-2 text-xl font-bold text-green-500'>Chemical Treatment</p>
+
+               </div>
+               
+                <ul>  
+                
+                
+                {diagnose.First_Suggested_Disease_Treatment.chemical.map((treatment, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {treatment}
+    </ListItem>
+  ))}
+                
+                
+                </ul> </>}
+               
+               
+               </>
+                
+                )} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
+              
+                 Treatment
+               </button>
+               <Modal
+          open={modalContent !==null}
+          onClose={handleCloseModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 'auto', // Adjust width as needed or set a specific value
+            maxWidth: '90%', // Prevents the modal from being too wide on small screens
+            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 4,
+            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
+          }}>
+            <IconButton
+              aria-label="close"
+              onClick={handleCloseModal}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Details
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            </Typography>
+          </Box>
+        </Modal>     
+          </div>
+          <div className='card  p-4 self-stretch'>
+            <Box display="flex" alignItems="center">
+              <Box width="100%" mr={1}>
+                <LinearProgress variant="determinate" value={probabilityPercent} sx={{'& .MuiLinearProgress-bar': { backgroundColor: '#EF4444' }, }} />
+              </Box>
+              <Box minWidth={35}>
+                <Typography variant="body2" sx={{fontWeight:'bold',color:'black'}}>{`${Math.round(probabilityPercent)}%`}</Typography>
+              </Box>
+            </Box>
+          </div>
+          
+        </div>
+
+        {diagnose.Second_Suggested_Disease_Name && (
+          <div className='flex flex-col items-start justify-between  p-4 '>
+            <div className='flex items-center pb-4'>
+              <img src={HeartDiseaseIcon} className='h-7 w-7' alt="Heart Disease Icon" />
+              <p className='font-bold text-gray-700 ml-2 text-lg'>{diagnose.Second_Suggested_Disease_Name}</p>
+              <img src={InformationCircle} className='h-5 w-5 ml-3' alt="Visibility Icon" />
+              <button onClick={() => handleOpenModal(<><div className='flex items-center'>
                 <InfoIcon style={{color:'#10B981'}} />
                 <p className='text-gray-700 font-bold text-xl ml-2'>Disease Description</p>
                 
@@ -339,7 +378,126 @@ function DiagnoseResult() {
 
               
 
-              </>}
+              </>)} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
+                 Details
+               </button>
+               <LocalHospitalIcon />
+               <button onClick={() => handleOpenModal(<>
+               
+               {diagnose.Second_Suggested_Disease_Treatment.biological && <>
+               
+               
+               <div className='flex items-center -ml-2'>
+                  <RecyclingIcon />
+                  <p className='ml-2 text-xl font-bold text-green-500'>Biological Treatment</p>
+
+               </div>
+               
+                <ul>  
+                
+                
+                {diagnose.Second_Suggested_Disease_Treatment.biological.map((treatment, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {treatment}
+    </ListItem>
+  ))}
+                
+                
+                </ul> </>}
+               
+               
+                {diagnose.Second_Suggested_Disease_Treatment.chemical && <>
+               
+               
+               <div className='flex items-center -ml-2 mt-2'>
+                  <ScienceIcon />
+                  <p className='ml-2 text-xl font-bold text-green-500'>Chemical Treatment</p>
+
+               </div>
+               
+                <ul>  
+                
+                
+                {diagnose.Second_Suggested_Disease_Treatment.chemical.map((treatment, index) => (
+    <ListItem key={index} sx={{
+      position: 'relative',
+      pl: '20px',
+      '&::before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        backgroundColor: '#10B981',
+      }
+    }}>
+      {treatment}
+    </ListItem>
+  ))}
+                
+                
+                </ul> </>}
+               
+               
+               </>
+                
+                )} className="font-semibold text-gray-600 text-sm ml-1 mr-5">
+              
+                 Treatment
+               </button>
+               <Modal
+          open={modalContent !== null}
+          onClose={handleCloseModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 'auto', // Adjust width as needed or set a specific value
+            maxWidth: '90%', // Prevents the modal from being too wide on small screens
+            maxHeight: '90vh', // Limits the modal's height to 90% of the viewport height
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 4,
+            overflowY: 'auto', // Enables scroll for content that exceeds the modal's height
+          }}>
+            <IconButton
+              aria-label="close"
+              onClick={handleCloseModal}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Details
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {/* Here you can place the content or details you want to show in the modal */}
+              {modalContent}
             </Typography>
           </Box>
         </Modal>      
